5fb9b1f24a0bddc2abceef74236f1c2aad20d927
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-10-02 23:16:43 +0200

    Add app/models.py. Fix app/views.py

--- app/views.py
+++ app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Index page.")
+    return HttpResponse("<h1>Homework-5 index page.</h1>")
 
 
 def generate_teachers(request):
-    return HttpResponse("Generate_teachers page.")
+    return HttpResponse("<h1>Generate_teachers page.</h1>")
