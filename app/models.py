0f7372a0fb6f75972dc3b181503293adf845bd6e
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-10-03 00:05:12 +0200

    Update DB model + makemigration, migrate.

--- app/models.py
+++ app/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Teacher(models.Model):
     first_name = models.CharField(max_length=255)
-    second_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     subject = models.CharField(max_length=255)
