59a5e1243d5b38625e4cd17654ce86cc2ea5e8f5
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-10-07 16:54:14 +0200

    Fix import in generate_teachers.py

--- app/management/commands/generate_teachers.py
+++ app/management/commands/generate_teachers.py
@@ -10,10 +10,10 @@ class Command(BaseCommand):
     help = "Add the specify number of teachers to the database."
 
     def add_arguments(self, parser):
-        parser.add_argument("number", nargs="+", type=int)
+        parser.add_argument("number", type=int)
 
     def handle(self, *args, **options):
-        for i in options["number"]:
+        for i in range(options["number"]):
             t = Teacher.objects.create(
                 first_name=fake.first_name(),
                 last_name=fake.last_name(),
